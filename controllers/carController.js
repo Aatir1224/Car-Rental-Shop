9df86a2bf9a303ef39002e2d5d2c59b1e406b6bd
Aatir1224 <aatirqureshi1224@gmail.com>
2021-09-09 15:11:58 +0530

    added get single car, rented car, get invoice of rented car functionality

--- controllers/carController.js
+++ controllers/carController.js
@@ -27,3 +27,56 @@ module.exports.carsNotRented = (req, res) => {
     });
   });
 };
+
+module.exports.getSingleCar = (req, res) => {
+  let query = `SELECT * FROM cars WHERE id='${req.params.id}'`;
+  db.pool.query(query, function (err, row) {
+    if (err) {
+      console.log(err);
+      res.sendStatus(400);
+    }
+    return res.status(200).json({
+      valid: true,
+      data: row,
+    });
+  });
+};
+
+module.exports.rentedCars = (req, res) => {
+  console.log(req.body);
+  let query = `INSERT INTO rentedCars (CarId,CarName,Price,DaysRented) VALUES ('${req.body.id}','${req.body.Name}','${req.body.price}','${req.body.DaysRented}')`;
+  db.pool.query(query, function (err, row) {
+    if (err) {
+      console.log(err);
+      res.sendStatus(400);
+    } else {
+      let query = `UPDATE cars SET cars.isRented='${1}' WHERE cars.id='${
+        req.body.id
+      }'`;
+      db.pool.query(query, function (err, row) {
+        if (err) {
+          console.log(err);
+          res.sendStatus(400);
+        }
+        res.status(200).json({
+          valid: true,
+        });
+      });
+    }
+  });
+};
+
+module.exports.getInvoiceofRentedCars = (req, res) => {
+  console.log(req.params.id);
+  let query = `SELECT * FROM rentedCars WHERE rentedCars.CarId='${req.params.id}'`;
+  db.pool.query(query, function (err, row) {
+    if (err) {
+      console.log(err);
+      res.sendStatus(400);
+    }
+    return res.status(200).json({
+      valid: true,
+      data: row,
+    });
+  });
+};
--- routes/carRoute.js
+++ routes/carRoute.js
@@ -1,6 +1,15 @@
 const express = require("express");
 const router = express.Router();
-const { getAllCars, carsNotRented } = require("../controllers/carController");
+const {
+  getAllCars,
+  carsNotRented,
+  getSingleCar,
+  rentedCars,
+  getInvoiceofRentedCars,
+} = require("../controllers/carController");
 router.get("/", getAllCars);
 router.get("/notRented", carsNotRented);
+router.get("/:id", getSingleCar);
+router.post("/create/rent", rentedCars);
+router.get("/getRentedCars/:id", getInvoiceofRentedCars);
 module.exports = router;
